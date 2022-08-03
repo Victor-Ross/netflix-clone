65b2a3f7aaa2e442ce33ba9e45c968950d53aac3
Victor Ross <victor2ross@gmail.com>
2022-08-02 21:08:52 -0300

    Finished design and trailer player

--- components/components/Thumbnail.tsx
+++ components/components/Thumbnail.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image';
+import { useRecoilState } from 'recoil';
 import { Movie } from '../../@types/typings';
+import { modalState, movieState } from '../../atoms/modalAtom';
 import { w500Url } from '../../constants/movie';
 
 interface IProps {
@@ -9,10 +11,17 @@ interface IProps {
 }
 
 function Thumbnail({ movie }: IProps) {
+  const [showModal, setShowModal] = useRecoilState(modalState);
+  const [currentMovie, setCurrentMovie] = useRecoilState(movieState);
+
   return (
     <div
       className="relative h-28 min-w-[180px] cursor-pointer transition duration-200
     ease-out md:h-36 md:min-w-[260px] md:hover:scale-105"
+      onClick={() => {
+        setCurrentMovie(movie);
+        setShowModal(true);
+      }}
     >
       <Image
         src={`${w500Url}${movie.backdrop_path || movie.poster_path}`}
